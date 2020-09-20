bdc1b2c1d0fa60c1f223f81982067e1a0e2b0f5f
Kieran815 <kieran.milligan@gmail.com>
2020-09-19 21:05:19 -0500

    pushing before reset

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+/*
+TODOS:
+Change/Add Title, Description and Metadata to the game.
+Change style `src/style.scss` and add your own color palette.
+Add a button to reload a game after a loss.
+Make it possible to “select skill level” to determine the size of the grid and the number of mines.
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Board from './component/Board';
+import './style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Game extends React.Component {
+  state = {
+    height: 8,
+    width: 8,
+    mines: 10
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    const { height, width, mines } = this.state;
+    return (
+      <div className="game">
+        <Board height={height} width={width} mines={mines} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
